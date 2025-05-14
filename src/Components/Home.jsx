351c34e0ee53677660556684fb13e21ae7d80d66
iftakhersifat <hossensifat615@gmail.com>
2025-05-15 00:10:11 +0600

    added fetch data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [Card, setCard] = useState();
+    useEffect(() => {
+    fetch("http://localhost:3000/coffees")
+      .then(res => res.json())
+      .then(data => {
+        setCard(data);
+      });
+  }, []);
+    console.log(Card)
     return (
         <div>
             <h1>Home Section</h1>
