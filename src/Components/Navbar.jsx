6956a924624ffeb0bf1f06b598b5962a529387c2
iftakhersifat <hossensifat615@gmail.com>
2025-05-15 19:49:57 +0600

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="update">Update</Link>
-        </div>
+        <div className='flex justify-center items-center gap-4 mt-10 p-2' style={{ backgroundImage: "url('/assets/images/more/15.jpg')" }}>
+                <img className='w-14' src="/assets/images/more/logo1.png" fill="white" alt="" />
+                <h1 className='text-2xl font-bold text-white'>Espresso Emporium</h1>
+            </div>
     );
 };
 
