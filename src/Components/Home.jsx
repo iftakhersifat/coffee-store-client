8a8898bcd0d08c048a5ee346052a7094aa4c5f1d
iftakhersifat <hossensifat615@gmail.com>
2025-05-15 20:02:45 +0600

    added updated parts

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -19,6 +19,22 @@ const Home = () => {
                     card.map(cards=><CardDetails key={cards._id} card={card} setCard={setCard} cards={cards}></CardDetails>)
                 }
             </div>
+
+            <div className='text-center mt-10 mb-10'>
+            <p>Follow Us Now</p>
+            <h1 className='font-bold text-2xl mb-4'>Follow on Instagram</h1>
+            </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-2 mb-10'>
+                <img src="/assets/images/cups/Rectangle 9.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 10.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 11.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 12.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 13.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 14.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 15.png" alt="" />
+                <img src="/assets/images/cups/Rectangle 16.png" alt="" />
+            </div>
         </div>
     );
 };
