31af0e906c932dbf5d33929f791599631134ebc4
iftakhersifat <hossensifat615@gmail.com>
2025-05-16 21:36:44 +0600

    added delete from db

--- src/Components/UserList/UserList.jsx
+++ src/Components/UserList/UserList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
 const UserList = () => {
 
@@ -12,7 +13,38 @@ const UserList = () => {
     },[])
     
     const handelDeleteButton =(_id)=>{
-        console.log("deleted this user", _id)
+        console.log("deleted this user", _id);
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          console.log(result.isConfirmed)
+          if (result.isConfirmed) {
+            fetch(`http://localhost:3000/users/${_id}`, {
+                  method: 'DELETE',
+                })
+                .then(res=>res.json())
+                .then(data=>{
+                         if (data.deletedCount) {
+                      Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+                    }
+        
+                    // remove the coffee from the state
+                    // const remainingUsers = card.filter(user => user._id !== _id)
+                    //     setCard(remainingUsers)
+                    
+                })
+          }
+        });
     }
 
     return (
